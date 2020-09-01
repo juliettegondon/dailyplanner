640ac97137ce2c942a64cd0e9f4b39f5b653937b
Juliette Gondon <juliettegondon@gmail.com>
2020-09-01 18:15:06 -0400

    set up motion.js for current day and time

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,14 @@
-//display current day and time at the top using moment.js
+//display current day and time at the top using moment.js & jquery. 
+//I added a separate ID for time so they could be stacked
+
+$("#currentDay").text("Today's date is " + moment().format('MMMM Do YYYY'));
+$("#currentTime").text("it is currently " + moment().format('h:mm:ss a'));
+
+//add currentTime var for color coding functionality
+var currentTime = moment().hours();
+
+
 //color-code time blocks past/present/future
+
+
 //store user input in local storage
