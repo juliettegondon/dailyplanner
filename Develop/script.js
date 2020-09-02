af8066fedea4e2f1415f4b7a1c6d5524ccebc81d
Juliette Gondon <juliettegondon@gmail.com>
2020-09-02 18:25:23 -0400

    got for loop working for color code

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,26 @@
 //display current day and time at the top using moment.js & jquery. 
-//I added a separate ID for time so they could be stacked
+//I added a separate ID for time so they could be stacked in the jumbotron
 
 $("#currentDay").text("Today's date is " + moment().format('MMMM Do YYYY'));
-$("#currentTime").text("it is currently " + moment().format('h:mm:ss a'));
+$("#currentTime").text("It is currently " + moment().format('h:mm:ss a'));
 
-//add currentTime var for color coding functionality
-var currentTime = moment().hours();
-
-
+//add presentTime var for color coding functionality. activity will indicate tense in a for loop
 //color-code time blocks past/present/future
+var presentTime = moment().hours();
+var activities = $(".activity");
+
+for (var i = 9; i < 18; i++) {
+    console.log(i, presentTime);
+    if ( i <presentTime) {
+        $('#' + i).addClass("past")
+    }
+    else if ( i === presentTime) {
+        $('#' + i).addClass("present")
+    }
+    else { $('#' + i).addClass("future")
+}
+}
 
 
 //store user input in local storage
+
